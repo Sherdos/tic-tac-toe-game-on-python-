3d3c5f9122e5525d87ced068af0910a6a6a0cb79
Sherdos <sherdos26@gmail.com>
2024-04-07 21:16:42 +0600

    second commmit add more new fiucher

--- main.py
+++ main.py
@@ -1,62 +1,96 @@
-import pygame, random
-
-
+import pygame
+import random
+import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Py game')
-icon = pygame.image.load('media/image/game.png')
-pygame.display.set_icon(icon)
+class TicTacToe:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((600, 600))
+        pygame.display.set_caption('Pygame Tic Tac Toe')
+        icon = pygame.image.load('media/image/game.png')
+        pygame.display.set_icon(icon)
+        self.clock = pygame.time.Clock()
+        self.myfont = pygame.font.Font('media/font/RobotoMono-VariableFont_wght.ttf', 40)
+        self.fields_group = pygame.sprite.Group()
+        self.create_fields()
+        self.current_player = 'cros'
 
-clock = pygame.time.Clock()
+    def create_fields(self):
+        for y in range(1, 4):
+            for x in range(1, 4):
+                field = Field('media/image/field.png', (100*x, 100*y), (x, y))
+                self.fields_group.add(field)
 
-square = pygame.Surface((50,170))
-square.fill('Blue')
+    def check_board(self):
+        all_win_positions = [
+            {(1, 1), (1, 2), (1, 3)},
+            {(2, 1), (2, 2), (2, 3)},
+            {(3, 1), (3, 2), (3, 3)},
+            {(1, 1), (2, 1), (3, 1)},
+            {(1, 2), (2, 2), (3, 2)},
+            {(1, 3), (2, 3), (3, 3)},
+            {(1, 1), (2, 2), (3, 3)},
+            {(3, 1), (2, 2), (1, 3)},
+        ]
+        cros_positions = [field.cordinate for field in self.fields_group if field.type == 1]
+        circle_positions = [field.cordinate for field in self.fields_group if field.type == 2]
+        for win_position in all_win_positions:
+            if win_position <= set(cros_positions) and set(cros_positions):
+                self.display_winner_message('cros wins!', 'Blue')
+                self.reset_game()
+            elif win_position <= set(circle_positions) and set(circle_positions):
+                self.display_winner_message('circle wins!', 'Red')
+                self.reset_game()
 
-myfont = pygame.font.Font('media/font/RobotoMono-VariableFont_wght.ttf',40)
-text_surface = myfont.render('Hello world',True,'Red')
+    def display_winner_message(self, message, color):
+        text_surface = self.myfont.render(message, True, color)
+        self.screen.blit(text_surface, (10, 10))
+        pygame.display.update()
+        time.sleep(2)
 
+    def reset_game(self):
+        self.fields_group.empty()
+        self.create_fields()
 
-# field = pygame.image.load('media/image/field.png')
-# field_rect = field.get_rect(topleft=(10, 100)) 
+    def run(self):
+        running = True
+        while running:
+            self.screen.fill("Green")
+            self.fields_group.draw(self.screen)
+            self.clock.tick(60)
+            pygame.display.update()
 
-
-running = True
-
-order = True
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for field in self.fields_group:
+                        if field.rect.collidepoint(event.pos):
+                            if field.full:
+                                if self.current_player == 'cros':
+                                    field.image = pygame.image.load('media/image/cros.png')
+                                    field.type = 1
+                                    self.fields_group.draw(self.screen, field)
+                                    self.current_player = 'circle'
+                                else:
+                                    field.image = pygame.image.load('media/image/circle.png')
+                                    field.type = 2
+                                    self.fields_group.draw(self.screen,field)
+                                    self.current_player = 'cros'
+                                field.full = False
+                                self.check_board()
 
 class Field(pygame.sprite.Sprite):
-    def __init__(self, image, position):
+    def __init__(self, image, position, cordinate):
         super().__init__()
         self.image = pygame.image.load(image)
         self.rect = self.image.get_rect(topleft=position)
-        
-fields_group = pygame.sprite.Group()
+        self.cordinate = cordinate
+        self.type = 0  # 1 - cros, 2 - circle
+        self.full = True
 
-for y in range(1,4):
-    for x in range(1,4):
-        field = Field('media/image/field.png', (100*x, 100*y))
-        fields_group.add(field)
-
-while running:
-    
-    screen.fill("Green")
-    fields_group.draw(screen)
-    
-    clock.tick(60)
-    
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            for field in fields_group:
-                if field.rect.collidepoint(event.pos):
-                    if order:
-                        field.image = pygame.image.load('media/image/circle.png')
-                        order = False
-                    else:
-                        field.image = pygame.image.load('media/image/cros.png')
-                        order = True
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.run()
